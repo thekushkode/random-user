1fd7bcfaebebf3d92f43cc0e0bfe82f73441199c
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-06-10 13:08:39 -0400

    adds dropdowns & click function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
 import RandomUser from './components/RandomUser';
+// import { Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Random D-Bags</h1>
+        <h1>Sketchier Tinder</h1>
       </header>
       <main>
+        <div className="Users">
+          <RandomUser></RandomUser>
+          <RandomUser></RandomUser>
+          <RandomUser></RandomUser>
+        </div>
+        <div className="Users">
+          <RandomUser></RandomUser>
+          <RandomUser></RandomUser>
+          <RandomUser></RandomUser>
+        </div>
         <div className="Users">
           <RandomUser></RandomUser>
           <RandomUser></RandomUser>
--- src/components/randomUser.jsx
+++ src/components/randomUser.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './RandomUser.css';
-import propTypes from 'prop-types';
+//import propTypes from 'prop-types';
+import { Dropdown } from 'react-bootstrap';
 
 
 class RandomUser extends Component {
@@ -25,6 +26,9 @@ class RandomUser extends Component {
                     name: data.results[0].name.first + " " + data.results[0].name.last,
                     email: data.results[0].email,
                     phone: data.results[0].phone,
+                    sex: data.results[0].gender,
+                    age: data.results[0].dob.age,
+                    location: data.results[0].location.city + ", " + data.results[0].location.state + ", " + data.results[0].location.country,
                     imgUrl: data.results[0].picture.large,
                 })
             })
@@ -35,24 +39,37 @@ class RandomUser extends Component {
         console.log('component mounted');
     }
 
-    generateNewProfile() {
+    generateNewProfile = () => {
         this.getNewUser();
         console.log('creating new user');
     }
 
-    ageSexLocation(e) {
-        this.setState({ enabled: !this.state.enabled });
-    }
-
     render() {
         return (
-            <div>
+            <div className="Person">
                 <h1>{this.state.name}</h1>
                 <img src={this.state.imgUrl} alt="" onClick={this.generateNewProfile} />
-                <h6 className="Asl" onClick={this.ageSexLocation}>
-                    <h6>{this.state.email}</h6>
-                    <h6>{this.state.phone}</h6>
-                </h6>
+                <Dropdown className="Dropdown">
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        A/S/L
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">{this.state.age}</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">{this.state.sex}</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">{this.state.location}</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown className="Dropdown">
+                    <Dropdown.Toggle variant="success" id="dropdown-primary-button">
+                        Contact
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">{this.state.email}</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">{this.state.phone}</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
         )
     }
